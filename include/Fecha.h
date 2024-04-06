5bf638583ad9e91edb04c4f81ac97f32e6332886
unknown <douglasarevalol21@gmail.com>
2024-04-06 08:24:11 -0600

    evalucion 1 lab.pro2

---
+++ include/Fecha.h
@@ -0,0 +1,20 @@
+#ifndef FECHA_H
+#define FECHA_H
+
+class Fecha
+{
+    public:
+        Fecha(int d, int m, int a);
+        virtual ~Fecha();
+
+        void mostrarFecha();
+
+    protected:
+
+    private:
+        int dia;
+        int mes;
+        int anio;
+};
+
+#endif // FECHA_H
